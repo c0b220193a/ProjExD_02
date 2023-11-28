f5ec8611d9348900df0e5d18d0aa6b568d2ccacd
c0b22019 <c0b220193a@edu.teu.ac.jp>
2023-11-28 14:32:47 +0900

    こうかとんの移動処理追加

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,12 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = {  #練習３　押下キーと移動量の辞書
+    pg.K_UP: (0, -5),  #横方向移動量、縦方向移動量
+    pg.K_DOWN: (0, 5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +18,9 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()  #こうかとんのれくとを抽出
+    kk_rct.center = (900, 400)  #こうかとんの初期座標
+
     bb_img = pg.Surface((20, 20))  #練習　１透明のSurfaceを作る
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  #練習１　半径10の赤色の円を作る
     bb_img.set_colorkey((0, 0, 0))
@@ -22,13 +31,23 @@ def main():
 
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for k, tpl in delta.items():  #キーが押された際にsum_mvに合計移動量が足される
+            if key_lst[k]:
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        screen.blit(kk_img, kk_rct)
+
         bb_rct.move_ip(vx, vy)  #練習２爆弾の移動処理
         screen.blit(bb_img, bb_rct)
         pg.display.update()
